89e7de8fe67363ae9e07ee9b7f59f3e5e8e11381
abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-01-04 12:52:47 +0000

    task 0: fixed errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import add_0
 if __name__ == "__main__":
+    """Print result of a and b"""
+    from add_0 import add
     a = 1
     b = 2
-    result = add_0.add(a, b)
+    result = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
