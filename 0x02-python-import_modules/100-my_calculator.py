aba595b3674c048c862c16d84a211ee20b674809
abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-01-04 11:59:33 +0100

    solved advanced task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
+
     count = len(sys.argv) - 1
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if sys.argv[2] == '+':
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     elif sys.argv[2] == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
-    
+
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
