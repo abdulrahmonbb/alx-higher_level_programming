afee9d08610b467a920184f553117a144efbd66f
abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-01-04 11:07:09 +0100

    solved task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,5 @@ if __name__ == "__main__":
 
     names = dir(hidden_4)
     for name in names:
-        if name.startswith('_'): 
-            continue
-        else:
+        if name[:2] != '__':
             print("{}".format(name))
