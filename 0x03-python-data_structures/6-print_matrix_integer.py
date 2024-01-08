6c102916a63687925e31e6c46b00823e269bc6db
abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-01-08 12:01:45 +0100

    task 6: i don tire

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,6 @@
 
 def print_matrix(matrix=[[]]):
 	for row in matrix:
-		print(" ".join("{:d}".format(num) for num in row))
+		for col in row:
+			print("{:d}".format(col), end=" " if col != row[-1] else "")
+		print()
