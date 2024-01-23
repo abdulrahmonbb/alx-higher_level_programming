7313680c4bd7d7163d930aeef23589b609438122
abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-01-23 00:41:29 +0000

    solved first task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,17 @@
-#!
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for elem in my_list:
+            if count < x:
+                print(elem, end=' ')
+                count+=1
+            else:
+                break
+
+        print()
+        return count
+    except:
+        print("An error occured while printing")
+        return (0)
