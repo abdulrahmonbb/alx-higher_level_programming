2a78c8ffaab9bfc6f6df0e3b14672e2bbea76d39
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-19 14:23:30 +0100

    6-multi_languages.js v1

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Prints 3 lines using an array and a for loop
 
-multi_languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
-  console.log(multi_languages[i]);
+  console.log(languages[i]);
 }
