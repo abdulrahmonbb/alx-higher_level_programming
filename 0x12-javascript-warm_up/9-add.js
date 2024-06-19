62bee2d6717b38e39419cac439755e66d06a7eff
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-19 13:49:18 +0100

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Prints the addition of two integers
+
+function add(a, b) {
+  a = process.argv[2];
+  b = process.argv[3];
+
+  console.log(parseInt(a) + parseInt(b));
+}
+
+add();
