e9b784b1da4d44359798119b9d5df35739b11c88
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-19 14:21:16 +0100

    7-multi_c.js v1

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,8 +6,8 @@ const x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing number of occurences');
 } else {
-	n = Number(x);
-	for (let i = 0; i < n; i++) {
-		console.log('C is fun');
-	}
+  const n = Number(x);
+  for (let i = 0; i < n; i++) {
+    console.log('C is fun');
+  }
 }
