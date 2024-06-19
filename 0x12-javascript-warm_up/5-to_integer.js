082a7ce2cbefb916c70389d1eac97150e25c9195
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-19 14:26:07 +0100

    5-to_integer.js v1

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const firstArg = process.argv[2]
+const firstArg = process.argv[2];
 
 if (isNaN(firstArg)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log(`My number: ${parseInt(firstArg)}`);
+  console.log(`My number: ${parseInt(firstArg)}`);
 }
