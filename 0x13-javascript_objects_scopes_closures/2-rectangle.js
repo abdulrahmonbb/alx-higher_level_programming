8f254666c7262bc2dfea1cfb0b29fc32cf01f882
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-21 06:23:56 +0100

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor(w, h) {
+      if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+        return;
+      }
+      else {
+        this.width = w;
+        this.height = h;
+      }
+    }
+};
