a39478d2f2c30b65162638e5633c7bc3f46f5133
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-21 11:27:34 +0100

    10-converter.js v1

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return function (number) {
-    if (number === 0) return '0';
-
-    const digits = '0123456789abcdefghijklmnopqrstuvwxyz';
-    let result = '';
-
-    while (number > 0) {
-      result = digits[number % base] + result;
-      number = Math.floor(number / base);
-    }
-
-    return result;
-  };
+  return num => num.toString(base);
 };
