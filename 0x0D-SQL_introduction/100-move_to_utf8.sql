2b3f340c057b64f86a0ef9adffafaee0002079f6
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-22 07:26:40 +0100

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Converts a database to UTF8 in our MySQL server
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
