7ca3a4e8c7063391f9a8e97264d7058aea43d61c
rahmoncodes <abdulrahmonbb@gmail.com>
2024-07-07 15:30:25 +0100

    fixed pycodestyle error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-This script  lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+This script  lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
