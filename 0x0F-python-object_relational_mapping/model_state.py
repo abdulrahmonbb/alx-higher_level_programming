c42958f8c9d70d9c69ce0095683473f1936d079b
rahmoncodes <abdulrahmonbb@gmail.com>
2024-07-08 13:55:17 +0100

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
+
 Base = declarative_base()
 
 class State(Base):
