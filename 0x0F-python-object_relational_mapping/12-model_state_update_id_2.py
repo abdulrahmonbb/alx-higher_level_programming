4e0c0ff11ad5b2597cbe33ad0f5117fd352b8b8f
rahmoncodes <abdulrahmonbb@gmail.com>
2024-07-09 11:35:22 +0100

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(
         argv[1], argv[2], argv[3]
     )
-    
+
     engine = create_engine(db_url)
 
     Session = sessionmaker(bind=engine)
