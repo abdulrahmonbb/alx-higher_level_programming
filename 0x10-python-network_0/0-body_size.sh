67c4666b5f309c847ab417e8103c19e4805fb7d1
Abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-07-15 06:55:06 -0700

    hsdwsjk

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # This script displays the response size in bytes of the request to a url
-echo $(curl -s -o /dev/null -w "%{size_download}" "$1")
+echo $(curl -s -o /dev/null -w "%{size_download}" "$1"
