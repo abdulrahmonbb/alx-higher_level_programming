9bfbc5c0b435873c7440eab0d8b156f65c7e61ba
Abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-07-16 02:30:43 -0700

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script takes a url and an email, sends a POST request
+to the passed url with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+url = argv[1]
+data = {'email': argv[2]}
+
+data_encoded = urllib.parse.urlencode(data)
+data_encoded = data_encoded.encode('ascii')
+
+req = urllib.request.Request(url, data_encoded)
+with urllib.request.urlopen(req) as response:
+	response_data = response.read().decode('utf-8')
+	print(response_data)	
