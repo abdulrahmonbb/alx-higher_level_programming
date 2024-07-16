3bf9c8f3463b0aaa3da70b1935149a24dd665da6
Abdulrahmonbb <abdulrahmonbb@gmail.com>
2024-07-16 02:40:09 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,13 +8,15 @@ import urllib.request
 import urllib.parse
 from sys import argv
 
-url = argv[1]
-data = {'email': argv[2]}
 
-data_encoded = urllib.parse.urlencode(data)
-data_encoded = data_encoded.encode('ascii')
+if __name__ == '__main__':
+    url = argv[1]
+    data = {'email': argv[2]}
 
-req = urllib.request.Request(url, data_encoded)
-with urllib.request.urlopen(req) as response:
-    response_data = response.read().decode('utf-8')
-    print(response_data)
+    data_encoded = urllib.parse.urlencode(data)
+    data_encoded = data_encoded.encode('ascii')
+
+    req = urllib.request.Request(url, data_encoded)
+    with urllib.request.urlopen(req) as response:
+        response_data = response.read().decode('utf-8')
+        print(response_data)
